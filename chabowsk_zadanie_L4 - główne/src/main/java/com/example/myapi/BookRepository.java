2d476e7598be6a8f52887abeee53e7e90915cf56
Kacper3321 <102919554+Kacper3321@users.noreply.github.com>
2023-11-12 20:09:10 +0100

    Delete chabowsk_zadanie_L4 - główne directory
    pomylka

--- chabowsk_zadanie_L4 - główne/src/main/java/com/example/myapi/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.myapi;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-// wykorzystujac metody JpaRepository jestem w stanie obsluzyc baze danych
-}
